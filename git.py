1ea10a9c888c2272e2bbf63d70c66b8973c98907
anshul-327 <chikkamath.anshul@gmail.com>
2026-06-08 21:09:08 +0530

    Did some editing in the explaination part, second commit

--- git.py
+++ git.py
@@ -12,13 +12,14 @@ while reply.lower() != "yes" and reply.lower() != "no":
             print("Good")
             print("First, I open the terminal and do into the directory of this file.")
             print("Then I type 'git init' to initialize the repository and like it to the repository in github.")
-            print("Then I set my branch to main by typing 'git branch -M main'")
+            print("Then I type 'git  remote add origin https://github.com/anshul-327/Push_testing.git' to see link the directory and github repository.")
             print("Then I add the files by typing 'git add .'")
-            print("Then I save a snapshot of my work by typing 'git commit -m First commit'(because it is first commit).")
-            print("Then I link it to the cloud using 'git remote add origin '")
-            print("Finally, I type 'git push -u origin main' to upload everything so you can see it!")
+            print("Then I save a snapshot of my work by typing 'git commit -m 'Added both the python files to the repository and this is the first commit'")
+            print("Finally, I type 'git push -u origin master' (first I used main but there was an error so I run git branch and saw it is master and not main) to upload everything so you can see it!")
         elif reply.lower() == "no":
             secret()
             # if you wnat to know what secret() does, just type no. Btw, it is my first time using a custom function that too imported!!!!
     else:
         print("That is not a vaild response")
+
+        # I also did a second commit so in the explaination there is only first commit
